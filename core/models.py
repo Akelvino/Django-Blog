a4023cd6ed22990eaa088b6756afdaee4ea78acd
Akelvino <vinho892@gmail.com>
2026-02-14 00:05:51 +0300

    Added the category model and registered it on admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural ='categories'
