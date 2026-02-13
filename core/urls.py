30c2d2bc7b84404c4f05136fac1ef1716d27b944
Akelvino <vinho892@gmail.com>
2026-02-14 00:13:07 +0300

    creted the settings media models

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
