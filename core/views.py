0d1a5080ac878ed00fd93c3ca73f939d67a328fd
Akelvino <vinho892@gmail.com>
2026-02-20 21:50:10 +0300

    Displayed the featured post

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Blog
 
 # Create your views here
 def home(request):
     categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured = True).order_by('update_at')
     context = {
-        'categories':categories
+        'categories':categories,
+        'featured_post':featured_post
     }
     return render(request, 'core/home.html', context)
